6fc3c76d3400d4a1001312ea21efefcdae3edede
AlexPt <alexpt1637@yandex.ru>
2018-07-16 23:46:19 +1000

    определение функции

--- untitled/test/js/main-2.js
+++ untitled/test/js/main-2.js
@@ -10,3 +10,29 @@ function display(){
     document.write("функция в JavaScript, произведение двух чисел z = " + z);
 }
 display();
+
+function newplay(){
+
+    var z = x / y;
+    document.write(", частное двух чисел z = " + z);
+}
+newplay();
+
+function display2(x){  // определение функции
+ 
+    var z = x * x;
+    document.write(", " + x + " в квадрате равно " + z);
+}
+ 
+display2(x); // вызов функции
+display2(5);
+display2(12);
+display2(y);
+
+function sum(a, b, c){
+    let d = a + b + c;
+    document.write(", d = " + d);
+}
+sum(1, 2, 3);
+let nums = [4, 5, 6];
+sum(...nums);
